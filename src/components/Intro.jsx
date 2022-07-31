42fb0434f65084c5eedbb23df2d9ea09e15ee664
JC Camara <80168125+Jiseeeh@users.noreply.github.com>
2022-07-31 15:23:25 +0800

    add: intro

---
+++ src/components/Intro.jsx
@@ -0,0 +1,9 @@
+export default function Intro(props) {
+  return (
+    <section className="intro">
+      <h1>Quizzy</h1>
+      <h2>Break your limits!</h2>
+      <input type="button" value="Start Quizzy" onClick={props.handleClick} />
+    </section>
+  );
+}
