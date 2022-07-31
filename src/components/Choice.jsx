3e3987e4fa746a7bca8d076ab7d90b6c72fb09f7
JC Camara <80168125+Jiseeeh@users.noreply.github.com>
2022-07-31 23:31:40 +0800

    add: classname and import

--- src/components/Choice.jsx
+++ src/components/Choice.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import "./Choice.css";
 export default function Choice(props) {
   return (
     <>
       <input
+        className="choice"
         type="radio"
         id={props.choice}
         name={props.name}
