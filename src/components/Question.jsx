deaba803f68371481f4755c4501f6733037f79fa
JC Camara <80168125+Jiseeeh@users.noreply.github.com>
2022-07-31 21:11:11 +0800

    add: html-entities

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { decode } from "html-entities";
 
 export default function Question(props) {
   return (
     <div className="question-container">
-      <h1>{props.question}</h1>
+      <h1>{decode(props.question)}</h1>
       <div className="choices">{props.choices}</div>
     </div>
   );
