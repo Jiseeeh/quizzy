b85af3e615311604a3b0e09c41cb283a863ca484
JC Camara <80168125+Jiseeeh@users.noreply.github.com>
2022-07-31 21:07:29 +0800

    add: rendering of questions

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import Question from "./Question";
 import "./Quiz.css";
+import Choice from "./Choice";
 
-export default function Quiz() {
+export default function Quiz(props) {
+  const questions = props.data.map((data) => {
+    const choices = data.choices.map((choice) => {
+      return <Choice choice={choice} name={data.name} />;
+    });
+    return (
+      <Question key={data.id} question={data.question} choices={choices} />
+    );
+  });
   return (
     <main className="quiz">
-      <h1>Test Quiz</h1>
+      {questions}
+      <button>Show Answers</button>
     </main>
   );
 }
